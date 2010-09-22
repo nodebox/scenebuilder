0d6d75363f683e89fbd3d4f55c931edcd82a0250
stebanos <stefan.gabriels@gmail.com>
2010-09-22 14:30:29 +0200

    Added Overflow port to ConvertRange node.

--- builtins/src/nodebox/builtins/math/ConvertRange.java
+++ builtins/src/nodebox/builtins/math/ConvertRange.java
@@ -12,6 +12,7 @@ public class ConvertRange extends Node {
     public FloatPort pSourceMaximum = new FloatPort(this, "sourceMaximum", Port.Direction.INPUT, 100);
     public FloatPort pTargetMinimum = new FloatPort(this, "targetMinimum", Port.Direction.INPUT, 0);
     public FloatPort pTargetMaximum = new FloatPort(this, "targetMaximum", Port.Direction.INPUT, 255);
+    public StringPort pOverflow = new StringPort(this, "overflow", Port.Direction.INPUT, "clamp");
     public FloatPort pOutputValue = new FloatPort(this, "outputValue", Port.Direction.OUTPUT);
 
     @Override
@@ -20,12 +21,20 @@ public class ConvertRange extends Node {
         float srcMax = pSourceMaximum.get();
         float targetMin = pTargetMinimum.get();
         float targetMax = pTargetMaximum.get();
-        float value = clamp(pValue.get(), srcMin, srcMax);
-        // Convert value to 0.0-1.0 range.
-        try {
-            value = (value - srcMin) / (srcMax - srcMin);
-        } catch (ArithmeticException e) {
-            value = srcMin;
+        String overflow = pOverflow.get();
+        float value = pValue.get();
+        if (overflow.equals("wrap")) {
+            value = srcMin + value % (srcMax - srcMin);
+        } else if (overflow.equals("mirror")) {
+            float rest = value % (srcMax - srcMin);
+            if ((int) (value / (srcMax - srcMin)) % 2 == 1)
+                value = srcMax - rest;
+            else
+                value = srcMin + rest;
+        } else if (overflow.equals("clamp")) {
+            value = clamp(value, srcMin, srcMax);
+        } else {
+            // ignore
         }
         // Convert value to target range.
         value = targetMin + value * (targetMax - targetMin);
