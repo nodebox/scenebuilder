98bfd76ba4e6f0a1179e826e78e14fe5b16a4a50
Frederik De Bleser <frederik@burocrazy.com>
2010-10-28 13:20:11 +0200

    Rewritten posterize node to make use of caching.
    Before, the levels cache was invalidated for each execution. The current code speeds up posterization.

--- pixie/src/nodebox/pixie/Posterize.java
+++ pixie/src/nodebox/pixie/Posterize.java
@@ -9,38 +9,30 @@ import nodebox.node.Port;
 @Category("Image")
 public class Posterize extends PointFilter {
 
-    public final IntPort pValue = new IntPort(this, "value", Port.Direction.INPUT, 5);
-    private int numLevels;
-    private int[] levels;
-    private boolean initialized = false;
+    public final IntPort pLevels = new IntPort(this, "levels", Port.Direction.INPUT, 5);
+    private int levels = -1;
+    private int[] levelArray;
 
-
-    public void setNumLevels() {
-        this.numLevels = pValue.get();
-        initialized = false;
-    }
-
-    public void createLevels() {
-        setNumLevels();
-        levels = new int[256];
-        if (numLevels != 1)
+    public void createLevels(int levels) {
+        this.levels = levels;
+        levelArray = new int[256];
+        if (levels != 1)
             for (int i = 0; i < 256; i++)
-                levels[i] = 255 * (numLevels * i / 256) / (numLevels - 1);
+                levelArray[i] = 255 * (levels * i / 256) / (levels - 1);
     }
 
     @Override
     public int filter(int x, int y, int v) {
-        if (!initialized) {
-            initialized = true;
-            createLevels();
+        if (levels != pLevels.get()) {
+            createLevels(pLevels.get());
         }
         int a = v & 0xff000000;
         int r = (v >> 16) & 0xff;
         int g = (v >> 8) & 0xff;
         int b = v & 0xff;
-        r = levels[r];
-        g = levels[g];
-        b = levels[b];
+        r = levelArray[r];
+        g = levelArray[g];
+        b = levelArray[b];
         return a | (r << 16) | (g << 8) | b;
     }
 
