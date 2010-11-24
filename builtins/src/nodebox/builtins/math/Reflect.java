5f110e1b10d17aa985816339d1459d440d5bbda1
stebanos <stefan.gabriels@gmail.com>
2010-11-24 12:46:27 +0100

    Chosen variable names that reflect better the intention of the ports purposes.

--- builtins/src/nodebox/builtins/math/Reflect.java
+++ builtins/src/nodebox/builtins/math/Reflect.java
@@ -2,29 +2,29 @@ package nodebox.builtins.math;
 
 import nodebox.node.*;
 
-@Description("Calculate the angle between two points.")
+@Description("Calculate the reflection of a point through an origin point.")
 @Category("Math")
 public class Reflect extends Node {
 
-    public FloatPort p1X = new FloatPort(this, "x1", Port.Direction.INPUT);
-    public FloatPort p1Y = new FloatPort(this, "y1", Port.Direction.INPUT);
-    public FloatPort p2X = new FloatPort(this, "x2", Port.Direction.INPUT);
-    public FloatPort p2Y = new FloatPort(this, "y2", Port.Direction.INPUT);
-    public FloatPort pd = new FloatPort(this, "d", Port.Direction.INPUT, (float) 1.0);
-    public FloatPort pa = new FloatPort(this, "a", Port.Direction.INPUT, 180);
-    public FloatPort pOutputValue = new FloatPort(this, "x", Port.Direction.OUTPUT);
-    public FloatPort pOutputValue2 = new FloatPort(this, "y", Port.Direction.OUTPUT);
+    public FloatPort pX1 = new FloatPort(this, "x1", Port.Direction.INPUT);
+    public FloatPort pY1 = new FloatPort(this, "y1", Port.Direction.INPUT);
+    public FloatPort pX2 = new FloatPort(this, "x2", Port.Direction.INPUT);
+    public FloatPort pY2 = new FloatPort(this, "y2", Port.Direction.INPUT);
+    public FloatPort pDistancePct = new FloatPort(this, "distance%", Port.Direction.INPUT, 100);
+    public FloatPort pAngleOffset = new FloatPort(this, "angleOffset", Port.Direction.INPUT, 180);
+    public FloatPort pOutputX = new FloatPort(this, "x", Port.Direction.OUTPUT);
+    public FloatPort pOutputY = new FloatPort(this, "y", Port.Direction.OUTPUT);
 
     @Override
     public void execute(Context context, float time) {
-        float dtemp = (float) Math.sqrt(Math.pow(p1X.get() - p2X.get(), 2) + Math.pow(p1Y.get() - p2Y.get(), 2));
-        float newd = (float) (pd.get() * dtemp);
-        float atemp = (float) Math.toDegrees(Math.atan2(p2Y.get() - p1Y.get(), p2X.get() - p1X.get()));
-        float newa = (float) (pa.get() + atemp);
-        float x1 = (float) (p1X.get() + Math.cos(Math.toRadians(newa)) * newd);
-        float y1 = (float) (p2Y.get() + Math.sin(Math.toRadians(newa)) * newd);
-        pOutputValue.set(x1);
-        pOutputValue2.set(y1);
+        float distance = (float) Math.sqrt(Math.pow(pX1.get() - pX2.get(), 2) + Math.pow(pY1.get() - pY2.get(), 2));
+        float newDistance = (float) (pDistancePct.get() / 100 * distance);
+        float angle = (float) Math.toDegrees(Math.atan2(pY2.get() - pY1.get(), pX2.get() - pX1.get()));
+        float newAngle = (float) (pAngleOffset.get() + angle);
+        float x = (float) (pX1.get() + Math.cos(Math.toRadians(newAngle)) * newDistance);
+        float y = (float) (pY2.get() + Math.sin(Math.toRadians(newAngle)) * newDistance);
+        pOutputX.set(x);
+        pOutputY.set(y);
     }
 
 }
