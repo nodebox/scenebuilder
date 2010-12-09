6fbe9390fc8a3865f65a152a248641c7f49895d5
stebanos <stefan.gabriels@gmail.com>
2010-12-09 14:11:08 +0100

    Adapted some tests to make them pass again.

--- core/test/nodebox/node/ColorPortTest.java
+++ core/test/nodebox/node/ColorPortTest.java
@@ -7,15 +7,21 @@ import java.awt.*;
 public class ColorPortTest extends TestCase {
 
     public void testValueAsString() {
-        ColorPort p = new ColorPort(null, "c", Port.Direction.INPUT, Color.RED);
+        ColorPort p = new ColorPort(new TestNode(), "c", Port.Direction.INPUT, Color.RED);
         assertEquals("#ff0000ff", p.getValueAsString());
         p.set(new Color(0x112233));
         assertEquals("#112233ff", p.getValueAsString());
     }
 
     public void testParseValue() {
-        ColorPort p = new ColorPort(null, "c", Port.Direction.INPUT, Color.BLACK);
+        ColorPort p = new ColorPort(new TestNode(), "c", Port.Direction.INPUT, Color.BLACK);
         assertEquals(Color.RED, p.parseValue("#ff0000ff"));
         assertEquals(new Color(0x112233), p.parseValue("#112233ff"));
+    }
+
+    public static class TestNode extends Node {
+        @Override
+        public void execute(Context context, float time) {
+        }
     }
 }
--- core/test/nodebox/node/FloatPortTest.java
+++ core/test/nodebox/node/FloatPortTest.java
@@ -7,8 +7,14 @@ public class FloatPortTest extends TestCase {
      * Test if we can also set integers.
      */
     public void testSetValue() {
-        FloatPort p = new FloatPort(null, "float", Port.Direction.INPUT, 0f);
+        FloatPort p = new FloatPort(new TestNode(), "float", Port.Direction.INPUT, 0f);
         p.setValue(12);
         assertEquals(12f, p.getValue());
+    }
+
+    public static class TestNode extends Node {
+        @Override
+        public void execute(Context context, float time) {
+        }
     }
 }
